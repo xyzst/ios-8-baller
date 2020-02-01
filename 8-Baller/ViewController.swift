ff0e2e9d80e18cac76feba8a41a9d5acc09c138a
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-01 12:52:29 -0600

    Initialize Xcode/Swift Project
    Baseline project for simulating a "magic eight ball." Include basic assets (answers).
    
    Related to: none

---
+++ 8-Baller/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  8-Baller
+//
+//  Created by Darren Rambaud on 02/01/2020.
+//  Copyright © 2019 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let answers = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+
+
+
+}
+
